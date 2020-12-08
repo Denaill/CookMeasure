f0326fdf01f7fac72415cda825d874e53d35e606
Denaill <danielrojas0198@gmail.com>
2020-12-08 16:41:33 -0500

    Final commit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import bcrypt
 import ssl
 import os
 import math
+import sys
 
 app = Flask(__name__)
 
@@ -19,9 +20,14 @@ app.config['MONGO_URI'] = 'mongodb+srv://admin:utp123@cluster0-tjpda.mongodb.net
 mongo = PyMongo(app)
 fs = gridfs.GridFS(mongo.db)
 
-@app.route('/pwabuilder-sw.js', methods=['GET'])
-def sw():
-    return send_from_directory('static/js','pwabuilder-sw.js')
+@staticmethod
+def all_paginated(page=1, per_page=8):
+    return Post.query.order_by(Post.created.asc()).\
+        paginate(page=page, per_page=per_page, error_out=False)
+
+#@app.route('/pwabuilder-sw.js', methods=['GET'])
+#def sw():
+#    return send_from_directory('static/js','pwabuilder-sw.js')
 
 @app.route('/',methods=['POST','GET'])
 def index():
@@ -49,17 +55,37 @@ def admin():
             imgs[str(c)]=i.filename
             c += 1
             print('* ',i)
+        separar_ing = request.form['ingredientes'].split(', ')
         new_receta = {'usuario':request.form['usuario'],
                     'title':request.form['title'],
                     'tiempo':request.form['tiempo'],
                     'description':request.form['description'],
-                    #'ingredientes':request.form['ingredientes'],
+                    'ingredientes':separar_ing,
                     'preparacion':request.form['preparacion'],
                     'link':request.form['link'],'images':imgs}
         _id = recetas.insert_one(new_receta)
+        print (new_receta)
         return redirect('recetas')
     else:
         return render_template('admin.html',recetas=mongo.db.recetas.find())
+
+@app.route('/comentar',methods=['GET','POST'])
+def comentar():
+    comentarios = mongo.db.comentarios
+    recetas = mongo.db.recetas
+    id = request.form['receta']
+    if request.method == 'POST':
+        comentario = {'usuario':request.form['usuario'],
+                    'receta':request.form['receta'],
+                    'comentario':request.form['comentario']
+                    }
+        _id = comentarios.insert_one(comentario)
+    target = recetas.find_one({'_id':ObjectId(id)})
+    return render_template('recetas.html',receta=target, comentarios=mongo.db.comentarios.find() )
+
+@app.route('/input')
+def input():
+    return render_template('input.html')
 
 @app.route('/images/<filename>')
 def image(filename):
@@ -72,29 +98,28 @@ def image(filename):
 def delete(item):
     recetas = mongo.db.recetas
     user = mongo.db.users
-    fsf = mongo.db.fs.files
-    fsc = mongo.db.fs.chunks
-    target = recetas.find_one({'_id':ObjectId(item)})
-    for i in target['images']:
-        obj = fsf.find_one({'filename':target['images'][i]})
-        fsf.find_one_and_delete({'filename':target['images'][i]})
-        fsc.delete_many({'files_id':obj['_id']})
+    comentarios = mongo.db.comentarios
     target = recetas.find_one_and_delete({'_id':ObjectId(item)})
     if user.email == 'administrador@cookmeasure.com':
         return redirect(url_for('recetas'))
     else:
         return redirect(url_for('misrecetas'))
 
+
 @app.route('/recetas')
 def recetas():
     recetas = mongo.db.recetas
-    return render_template('receta.html',recetas=recetas.find())
+    rep = []
+    comentarios = mongo.db.comentarios
+    return render_template('receta.html',recetas=recetas.find(), ingres=recetas.find(), especifico=recetas.find(), ingro = rep)
 
 @app.route('/recetas/<receta>')
 def receta1(receta):
     recetas = mongo.db.recetas
+    comentarios = mongo.db.comentarios
     target = recetas.find_one({'_id':ObjectId(receta)})
-    return render_template('recetas.html',receta=target)
+    coment = comentarios.find({'receta':ObjectId(receta)})
+    return render_template('recetas.html',receta=target, comentarios=mongo.db.comentarios.find())
 
 @app.route('/misrecetas')
 def misrecetas():
@@ -176,6 +201,38 @@ def logout():
 def logint():
     return render_template('login.html')
 
+@app.route('/busqueda',methods=['POST'])
+def busqueda():
+    recetas = mongo.db.recetas
+    if request.method == 'POST':
+        buscar = request.form['busqueda']
+        resultado = recetas.find()
+    return render_template('busqueda.html',recetas=resultado, busqueda=buscar)
+
+@app.route('/especifico',methods=['POST'])
+def especifico():
+    recetas = mongo.db.recetas
+    if request.method == 'POST':
+        ings = request.form.getlist('ingredientes_check')
+        rep = []
+        resultado = recetas.find()
+        tamanio = len(ings)
+        repetido = recetas.find({'ingredientes':ings})
+        for i in repetido:
+            rep.append(i)
+    return render_template('especifica.html', recetas=resultado, tamanio = rep, ingredientes_especifico=ings, ingres=recetas.find(), especifico=recetas.find())
+
+@app.route('/filtro', methods=['POST'])
+def filtro():
+    recetas = mongo.db.recetas
+    if request.method == 'POST':
+        ings = request.form.getlist('ingredientes_check')
+        resultado = recetas.find()
+    return render_template('filtro.html', recetas=resultado, ingredientes_especifico=ings, ingres=recetas.find(), especifico=recetas.find())
+
+@app.route('/creadores')
+def creadores():
+    return render_template('creadores.html')
 
 if __name__ == '__main__':
     app.secret_key = 'mykey'
